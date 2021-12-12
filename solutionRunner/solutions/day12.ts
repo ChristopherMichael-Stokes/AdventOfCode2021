59282fec61b03accd7bbead620edc853747b03e0
ChristopherMichael-Stokes <christophermichaelstokes@gmail.com>
2021-12-12 12:33:27 +0000

    Parse input

--- solutionRunner/solutions/day12.ts
+++ solutionRunner/solutions/day12.ts
@@ -1,11 +1,15 @@
 import Solution from './solution';
 
 class Day12 extends Solution {
+    //private readonly graph: number[][];
     constructor() {
         super(12);
         if (!this.input.inputString) {
             throw new Error('Error reading input');
         }
+        // First enumerate all node names
+        let locations = [... new Set(this.input.inputString.split('\n').map(line => line.split('-')).reduce((accumulator, value) => accumulator.concat(value)))];
+        console.log(locations);
     }
 
     public part1(): string {
